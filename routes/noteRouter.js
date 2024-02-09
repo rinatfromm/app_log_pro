7ff97facb1a2677e3654aae8364be4c2ca20b804
Rinat Kov <frommrinat@gmail.com>
2024-02-09 11:25:27 +0100

    finished

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -1,15 +1,14 @@
-const router = require('express').Router()
-const auth = require('../middleware/auth')
-const noteCtrl = require('../controllers/noteCtrl')
+const router = require("express").Router();
+const auth = require("../middleware/auth");
+const noteCtrl = require("../controllers/noteCtrl");
 
-router.route('/')
-    .get(auth, noteCtrl.getNotes)
-    .post(auth, noteCtrl.createNote)
+router.route("/")
+.get(auth, noteCtrl.getNotes)
+.post(auth, noteCtrl.createNote);
 
-router.route('/:id')
-    .get(auth, noteCtrl.getNote)
-    .put(auth, noteCtrl.updateNote)
-    .delete(auth, noteCtrl.deleteNote)
+router.route("/:id")
+.get(auth, noteCtrl.getNote)
+.put(auth, noteCtrl.updateNote)
+.delete(auth, noteCtrl.deleteNote);
 
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -6,43 +6,42 @@ const userRouter = require('./routes/userRouter')
 const noteRouter = require('./routes/noteRouter')
 const path = require('path')
 
-
 const app = express()
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
-
-// Routes
+//Routes
 app.use('/users', userRouter)
 app.use('/api/notes', noteRouter)
 
 
 // Connect to MongoDB
-const URI = process.env.MONGODB_URL
+const URI = process.env.MONGODB_URL;
+
 mongoose.connect(URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, err =>{
-    if(err) throw err;
-    console.log('Connected to MongoDB')
 })
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+});
 
 
-// Below MongoDB and  Above Listen Sever
+
+// Below MongoDB and Above Listen Server
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) =>{
+    app.use(express.static('client/build'))
+    app.get('*', (req,res)=> {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    });
+    })
 }
 
 
-
-
-// Listen Server
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+//Listen Server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=> {
     console.log('Server is running on port', PORT)
 })
